d6bfeb01eedf19a6292578ace481e47343a282ff
Hedi Ikherbane <hediikherbane@gmail.com>
2025-12-03 19:39:27 +0100

    Biblio Box: ajout de test_box_add

--- TestsBoxes.java
+++ TestsBoxes.java
@@ -1,10 +1,23 @@
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
-
-public class TestsBoxes{
+public class TestsBoxes {
     @Test
     public void testBoxCreate() {
         Box b = new Box();
     }
-};
+
+    /** on veut pouvoir mettre des Thing dedans */
+    @Test
+    public void testBoxAddThing() {
+        Box b = new Box();
+        Thing t1 = new Thing("truc1");
+        Thing t2 = new Thing("truc2");
+        b.add(t1);
+        b.add(t2);
+
+        assertEquals(2, b.contents.size());
+        assertEquals("truc1", b.contents.get(0).name);
+        assertEquals("truc2", b.contents.get(1).name);
+    }
+}
