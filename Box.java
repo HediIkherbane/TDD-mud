1b6b06e148de78a332b839ab504004a8a257d109
Hedi Ikherbane <hediikherbane@gmail.com>
2025-12-10 18:42:15 +0100

    Ajout du box remove

--- Box.java
+++ Box.java
@@ -6,4 +6,15 @@ class Box {
     public void add(Thing truc) {
         this.contents.add(truc);
     }
+
+    public boolean contains(Thing truc) {
+        return this.contents.contains(truc);
+    }
+
+    public void remove(Thing truc) throws Exception {
+        if (!this.contents.contains(truc)) {
+            throw new Exception("ca n'existe pas");
+        }
+        this.contents.remove(truc);
+    }
 }
--- TestsBoxes.java
+++ TestsBoxes.java
@@ -1,5 +1,5 @@
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TestsBoxes {
     @Test
@@ -21,6 +21,25 @@ public class TestsBoxes {
         assertEquals("truc2", b.contents.get(1).name);
     }
 
+
+    @Test
+    public void testBoxRemove() throws Exception {
+        Box b = new Box();
+        Thing t1 = new Thing("truc1");
+        b.add(t1);
+        
+        b.remove(t1);
+        
+     
+        assertFalse(b.contains(t1));
+    }
+
+    @Test(expected = Exception.class)
+    public void testBoxRemoveException() throws Exception {
+        Box b = new Box();
+        Thing t1 = new Thing("truc1");
+        b.remove(t1); 
+    }
     @Test
     public void testBoxContains() {
         Box b = new Box();
@@ -29,7 +48,6 @@ public class TestsBoxes {
         Thing t3 = new Thing("truc3");
         b.add(t1);
         b.add(t2);
-        b.add(t3);
 
         assertTrue(b.contains(t1));
         assertTrue(b.contains(t2));
